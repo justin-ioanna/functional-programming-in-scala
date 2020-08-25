e13faf880f00f89114503866c2130f92e1c24e5c
Justin Ioanna <justin.ioanna20@gmail.com>
2020-08-25 22:43:24 +0100

    Add chapter 3 progress

--- src/main/scala/chapter3/List.scala
+++ src/main/scala/chapter3/List.scala
@@ -1,6 +1,5 @@
 package chapter3
 
-
 sealed trait List[+A]
 case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
@@ -21,5 +20,61 @@ object List {
   def apply[A](as: A*): List[A] =
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
+
+  /**
+   * Exercise 3.2
+   */
+  def tail[A](x: List[A]) = x match {
+    case Cons(head, tail) => tail
+    case Nil => throw new NoSuchElementException
+  }
+
+  /**
+   * Exercise 3.3
+   */
+  def setHead[A](x: List[A], h: A) = x match {
+    case Cons(head, tail) => Cons(h, tail)
+    case Nil => throw new NoSuchElementException
+  }
+
+ /**
+   * Exercise 3.4
+   */
+  def drop[A](x: List[A], n: Int): List[A] =
+    if (n < 1) x
+    else x match {
+      case Cons(head, tail) => drop(tail, n - 1)
+      case Nil => Nil
+    }
+
+  /**
+   * Exercise 3.5
+   */
+  def dropWhile[A](x: List[A])(f: A => Boolean): List[A] = x match {
+    case Cons(head, tail) if f(head) => dropWhile(tail)(f)
+    case _ => x
+  }
+
+  def reverse[A](x: List[A]): List[A] = {
+    @annotation.tailrec
+    def loop(acc: List[A], x: List[A]): List[A] = x match {
+      case Cons(head, tail) => loop(Cons(head, acc), tail)
+      case Nil => acc
+    }
+    loop(Nil, x)
+  }
+
+  /**
+   * Exercise 3.6
+   */
+  def init[A](x: List[A]): List[A] = {
+    @annotation.tailrec
+    def loop(acc: List[A], x: List[A]): List[A] = x match {
+      case Cons(head, Nil) => reverse(acc)
+      case Cons(head, tail) => loop(Cons(head, acc), tail)
+      case Nil => Nil
+    }
+    loop(Nil, x)
+  }
 
 }
--- src/test/scala/chapter3/ListSuite.scala
+++ src/test/scala/chapter3/ListSuite.scala
@@ -43,11 +43,20 @@ class ListSuite extends AnyFunSuite {
   }
 
   test("dropWhile: dropWhile should drop elements up to the point where the predicate is no longer satisfied.") {
-    assert(dropWhile(List(1, 2, 3, 4, 5), (x: Int) => x < 3) == List(3, 4, 5))
+    assert(dropWhile(List(1, 2, 3, 4, 5))(x => x < 3) == List(3, 4, 5))
   }
 
   test("dropWhile: dropWhile on list with elements that all satisfy the predicate should return Nil") {
-    assert(dropWhile(List(1, 2, 3, 4, 5), (x: Int) => x < 6) == Nil)
+    assert(dropWhile(List(1, 2, 3, 4, 5))(x => x < 6) == Nil)
+  }
+
+  test("reverse: reverse of List(1, 2, 3) should be List(3, 2, 1)") {
+    assert(reverse(List(1, 2, 3)) === List(3, 2, 1))
+  }
+
+  test("reverse: reverse of empty list should be an empty list") {
+    assert(reverse(List()) === List())
+    assert(reverse(Nil) === Nil)
   }
 
   test("init: init on list should return list minus last element") {
@@ -58,63 +67,64 @@ class ListSuite extends AnyFunSuite {
     assert(init(Nil) == Nil)
   }
 
-  test("length: length of 5 element list should be 5") {
-    assert(length(List(1, 2, 3, 4, 5)) == 5)
-  }
+  // test("length: length of 5 element list should be 5") {
+  //   assert(length(List(1, 2, 3, 4, 5)) == 5)
+  // }
 
-  test("foldLeft: sum elements of list with foldLeft") {
-    assert(foldLeft(List(1, 2, 3, 4, 5), 0)(_ + _) == 15)
-  }
+  // test("foldLeft: sum elements of list with foldLeft") {
+  //   assert(foldLeft(List(1, 2, 3, 4, 5), 0)(_ + _) == 15)
+  // }
 
-  test("sumLeft: sum elements of list with foldLeft") {
-    assert(sumLeft(List(1, 2, 3, 4, 5)) == 15)
-  }
+  // test("sumLeft: sum elements of list with foldLeft") {
+  //   assert(sumLeft(List(1, 2, 3, 4, 5)) == 15)
+  // }
 
-  test("productLeft: product elements of list with foldLeft") {
-    assert(productLeft(List(1, 2, 3, 4, 5)) == 120)
-  }
+  // test("productLeft: product elements of list with foldLeft") {
+  //   assert(productLeft(List(1, 2, 3, 4, 5)) == 120)
+  // }
 
-  test("lengthLeft: length of list with foldLeft") {
-    assert(lengthLeft(List(1, 2, 3, 4, 5)) == 5)
-  }
+  // test("lengthLeft: length of list with foldLeft") {
+  //   assert(lengthLeft(List(1, 2, 3, 4, 5)) == 5)
+  // }
 
-  test("reverse: reverse list using foldLeft") {
-    assert(reverse(List(1, 2, 3, 4, 5)) == List(5, 4, 3, 2, 1))
-  }
+  // test("reverse: reverse list using foldLeft") {
+  //   assert(reverse(List(1, 2, 3, 4, 5)) == List(5, 4, 3, 2, 1))
+  // }
 
-  test("reverse: reverse empty list should return empty list") {
-    assert(reverse(List()) == List())
-  }
+  // test("reverse: reverse empty list should return empty list") {
+  //   assert(reverse(List()) == List())
+  // }
 
-  test("append: should add second list to end of first") {
-    assert(append(List(1, 2, 3), List(4, 5, 6)) == List(1, 2, 3, 4, 5, 6))
-  }
+  // test("append: should add second list to end of first") {
+  //   assert(append(List(1, 2, 3), List(4, 5, 6)) == List(1, 2, 3, 4, 5, 6))
+  // }
 
-  test("concatenate: should concatenate a list of lists") {
-    assert(concatenate(List(List(1, 2), List(3, 4), List(5, 6))) == List(1, 2, 3, 4, 5, 6))
-  }
+  // test("concatenate: should concatenate a list of lists") {
+  //   assert(concatenate(List(List(1, 2), List(3, 4), List(5, 6))) == List(1, 2, 3, 4, 5, 6))
+  // }
 
-  test("add1: should add 1 to each element in the list") {
-    assert(add1(List(1, 2, 3)) == List(2, 3, 4))
-  }
+  // test("add1: should add 1 to each element in the list") {
+  //   assert(add1(List(1, 2, 3)) == List(2, 3, 4))
+  // }
 
-  test("convertToString: should convert elements in list from Double to String") {
-    assert(convertToString(List(1.0, 2.0, 3.0)) == List("1.0", "2.0", "3.0"))
-  }
+  // test("convertToString: should convert elements in list from Double to String") {
+  //   assert(convertToString(List(1.0, 2.0, 3.0)) == List("1.0", "2.0", "3.0"))
+  // }
 
-  test("map: should apply a function to each element in a list") {
-    assert(map(List(1, 2, 3))(x => 2 * x) == List(2, 4, 6))
-  }
+  // test("map: should apply a function to each element in a list") {
+  //   assert(map(List(1, 2, 3))(x => 2 * x) == List(2, 4, 6))
+  // }
 
-  test("filter: should remove elements from list based on predicate") {
-    assert(filter(List(5, 4, 1, 2, 3))(x => x > 2) == List(5, 4, 3))
-  }
+  // test("filter: should remove elements from list based on predicate") {
+  //   assert(filter(List(5, 4, 1, 2, 3))(x => x > 2) == List(5, 4, 3))
+  // }
 
-  test("flatmap: should map then flatten") {
-    assert(flatMap(List(1, 2, 3))(x => List(x, x)) == List(1, 1, 2, 2, 3, 3))
-  }
+  // test("flatmap: should map then flatten") {
+  //   assert(flatMap(List(1, 2, 3))(x => List(x, x)) == List(1, 1, 2, 2, 3, 3))
+  // }
 
-  test("filterViaFlatMap: should filter list") {
-    assert(filterViaFlatMap(List(5, 4, 1, 2, 3))(x => x > 2) == List(5, 4, 3))
-  }
+  // test("filterViaFlatMap: should filter list") {
+  //   assert(filterViaFlatMap(List(5, 4, 1, 2, 3))(x => x > 2) == List(5, 4, 3))
+  // }
+
 }
