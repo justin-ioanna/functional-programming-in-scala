fb2d6da63fe97c8dd6311e78fd594cb36982cd5a
Justin Ioanna <justin.ioanna20@gmail.com>
2020-09-20 13:59:27 +0100

    Add chapter 5 solutions

--- src/main/scala/chapter5/Stream.scala
+++ src/main/scala/chapter5/Stream.scala
@@ -9,6 +9,9 @@ sealed trait Stream[+A] {
     case Empty      => None
   }
 
+  /**
+   * Exercise 5.1
+   */
   def toList: List[A] = {
     val buffer = new collection.mutable.ListBuffer[A]
     @annotation.tailrec
@@ -22,16 +25,23 @@ sealed trait Stream[+A] {
     loop(this)
   }
 
+  /**
+   * Exercise 5.2
+   */
   def take(n: Int): Stream[A] = this match {
     case Cons(h, t) if n > 1  => cons(h(), t().take(n - 1))
     case Cons(h, t) if n == 1 => cons(h(), empty)
     case _                    => empty
   }
 
+  /**
+   * Exercise 5.2
+   */
   def drop(n: Int): Stream[A] = this match {
     case Cons(h, t) if n > 0 => t().drop(n - 1)
     case _                   => this
   }
+
 
   def foldRight[B](z: => B)(f: (A, => B) => B): B = this match {
     case Cons(h, t) => f(h(), t().foldRight(z)(f))
@@ -40,19 +50,110 @@ sealed trait Stream[+A] {
 
   def exists(p: A => Boolean): Boolean = foldRight(false)((a, b) => p(a) || b)
 
+  /**
+   * Exercise 5.4
+   */
   def forAll(p: A => Boolean): Boolean = foldRight(true)((a, b) => p(a) && b)
 
+  /**
+   * Exercise 5.3 & 5.5
+   */
   def takeWhile(p: A => Boolean): Stream[A] = foldRight(empty[A])((a, b) => if (p(a)) cons(a, b) else empty)
 
+  /**
+   * Exercise 5.6
+   */
   def headOptionViaFoldRight: Option[A] = foldRight(None: Option[A])((head, _) => Some(head))
 
+  /**
+   * Exercise 5.7
+   */
   def map[B](f: A => B): Stream[B] = foldRight(empty[B])((a, b) => cons(f(a), b))
   
+  /**
+   * Exercise 5.7
+   */
   def filter(p: A => Boolean): Stream[A] = foldRight(empty[A])((a, b) => if (p(a)) cons(a, b) else b)
 
+  /**
+   * Exercise 5.7
+   */
   def append[B >: A](other: => Stream[B]): Stream[B] = foldRight(other)((a, b) => cons(a, b))
 
+  /**
+   * Exercise 5.7
+   */
   def flapMap[B](f: A => Stream[B]): Stream[B] = foldRight(empty[B])((a, b) => f(a).append(b))
+
+  /**
+   * Exercise 5.13
+   */
+  def mapViaUnfold[B](f: A => B): Stream[B] = unfold(this) {
+    case Cons(head, tail) => Some((f(head()), tail()))
+    case _ => None
+  }
+
+  /**
+   * Exercise 5.13
+   */
+  def takeViaUnfold(n: Int): Stream[A] = unfold((this, n)) {
+    case (Cons(head, tail), x) if x > 1  => Some((head(), (tail(), n - 1)))
+    case (Cons(head, tail), 1)           => Some((head(), (empty, n - 1)))
+    case _                               => None
+  }
+
+  /**
+   * Exercise 5.13
+   */
+  def takeWhileViaUnfold(p: A => Boolean): Stream[A] = unfold(this) {
+    case Cons(head, tail) if p(head()) => Some((head(), tail()))
+    case _                             => None
+  }
+
+  /**
+   * Exercise 5.13
+   */
+  def zipWith[B, C](other: Stream[B])(f: (A, B) => C): Stream[C] = unfold((this, other)) {
+    case (Cons(h1, t1), Cons(h2, t2)) => Some((f(h1(), h2()), (t1(), t2())))
+    case _                            => None
+  }
+
+  /**
+   * Exercise 5.13
+   */
+  def zipAll[B](other: Stream[B]): Stream[(Option[A], Option[B])] = unfold((this, other)) {
+    case (Cons(h1, t1), Cons(h2, t2)) => Some(((Some(h1()), Some(h2())), (t1(), t2())))
+    case (Cons(h1, t1), Empty)        => Some(((Some(h1()), None), (t1(), empty)))
+    case (Empty, Cons(h2, t2))        => Some(((None, Some(h2())), (empty, t2())))
+    case _                            => None
+  }
+
+  /**
+   * Exercise 5.14
+   */
+  def startsWith[A](other: Stream[A]): Boolean =
+    this.zipAll(other).takeWhile(!_._2.isEmpty).forAll({ case (a, b) => a == b })
+
+  /**
+   * Exercise 5.15
+   */
+  def tails: Stream[Stream[A]] = unfold(this) {
+    case Cons(head, tail) => Some((Cons(head, tail), tail()))
+    case _                => None
+  }
+
+  def hasSubsequence[A](s: Stream[A]): Boolean = tails.exists(_.startsWith(s))
+  
+  /**
+   * Exercise 5.16
+   */
+  def scanRight[B](z: => B)(f: (A, => B) => B): Stream[B] =
+    foldRight((z, Stream(z)))((a, p0) => {
+      lazy val p1 = p0
+      val b2 = f(a, p1._1)
+      (b2, cons(b2, p1._2))
+    })._2
+
 
 }
 
@@ -72,26 +173,50 @@ object Stream {
   def apply[A](as: A*): Stream[A] =
     if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
 
+  /**
+   * Exercise 5.8
+   */
   def constant[A](a: A): Stream[A] = cons(a, constant(a))
 
+  /**
+   * Exercise 5.9
+   */
   def from(n: Int): Stream[Int] = cons(n, from(n + 1))
 
+  /**
+   * Exercise 5.10
+   */
   def fibs: Stream[Int] = {
     def loop(a: Int, b: Int): Stream[Int] = cons(a, loop(b, a + b))
     loop(0, 1)
   }
 
+  /**
+   * Exercise 5.11
+   */
   def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = f(z) match {
     case Some((a, s)) => cons(a, unfold(s)(f))
     case None         => empty
   }
 
+  /**
+   * Exercise 5.12
+   */
   def fibsViaUnfold: Stream[Int] = unfold((0, 1))({ case (a, b) => Some(a, (b, a + b)) })
 
+  /**
+   * Exercise 5.12
+   */
   def fromViaUnfold(n: Int): Stream[Int] = unfold(n)(n => Some(n, n + 1))
 
+  /**
+   * Exercise 5.12
+   */
   def constantViaUnfold[A](a: A): Stream[A] = unfold(a)(_ => Some(a, a))
 
+  /**
+   * Exercise 5.12
+   */
   val onesViaUnfold: Stream[Int] = constantViaUnfold(1)
 
 }
--- src/test/scala/chapter5/StreamSuite.scala
+++ src/test/scala/chapter5/StreamSuite.scala
@@ -18,6 +18,11 @@ class StreamSuite extends AnyFunSuite {
     assert(stream.take(2).toList === List(1, 4))
   }
 
+  test("takeViaUnfold: take(n) should return first n elements of stream") {
+    val stream: Stream[Int] = cons(square(1), cons(square(2), cons(square(3), empty)))
+    assert(stream.takeViaUnfold(2).toList === List(1, 4))
+  }
+
   test("drop: drop(n) should drop first n elements of stream") {
     val stream: Stream[Int] = cons(square(1), cons(square(2), cons(square(3), empty)))
     assert(stream.drop(2).toList === List(9))
@@ -38,9 +43,19 @@ class StreamSuite extends AnyFunSuite {
     assert(stream.takeWhile(a => a % 2 == 0).toList === List(2, 4))
   }
 
+  test("takeWhileViaUnfold: should return elements up until predicate is no longer satisfied") {
+    val stream: Stream[Int] = cons(2, cons(4, cons(5, empty)))
+    assert(stream.takeWhileViaUnfold(a => a % 2 == 0).toList === List(2, 4))
+  }
+
   test("map: should apply f to elements of stream") {
     val stream: Stream[Int] = cons(2, cons(3, cons(4, empty)))
     assert(stream.map(a => a * a).toList === List(4, 9, 16))
+  }
+
+  test("mapViaUnfold: should apply f to elements of stream") {
+    val stream: Stream[Int] = cons(2, cons(3, cons(4, empty)))
+    assert(stream.mapViaUnfold(a => a * a).toList === List(4, 9, 16))
   }
 
   test("filter: should return stream containing elements that satisfy the predicate") {
@@ -53,5 +68,62 @@ class StreamSuite extends AnyFunSuite {
     val s2: Stream[Int] = cons(5, cons(6, cons(7, empty)))
     assert(s1.append(s2).toList === List(2, 3, 4, 5, 6, 7))
   }
+
+  test("constant: should return a stream of given A") {
+    assert(constant(5).take(5).toList === List(5, 5, 5, 5, 5))
+  }
+  
+  test("constantViaUnfold: should return a stream of given A") {
+    assert(constantViaUnfold(5).take(5).toList === List(5, 5, 5, 5, 5))
+  }
+
+  test("from: should return a stream of consecutive integers starting from n") {
+    assert(from(10).take(5).toList === List(10, 11, 12, 13, 14))
+  }
+
+  test("fromViaUnfold: should return a stream of consecutive integers starting from n") {
+    assert(fromViaUnfold(10).take(5).toList === List(10, 11, 12, 13, 14))
+  }
+
+  test("fibs: should return a stream of fibonacci numbers") {
+    assert(fibs.take(10).toList === List(0, 1, 1, 2, 3, 5, 8, 13, 21, 34))
+  }
+
+  test("fibsViaUnfold: should return a stream of fibonacci numbers") {
+    assert(fibsViaUnfold.take(10).toList === List(0, 1, 1, 2, 3, 5, 8, 13, 21, 34))
+  }
+
+  test("zipWith: should return a stream of fibonacci numbers") {
+    assert(fibsViaUnfold.take(10).toList === List(0, 1, 1, 2, 3, 5, 8, 13, 21, 34))
+  }
+
+  test("zipWith: should add corresponding elements of two lists") {
+    val s1: Stream[Int] = cons(1, cons(2, cons(3, cons(4, empty))))
+    val s2: Stream[Int] = cons(4, cons(5, cons(6, empty)))
+    assert(s1.zipWith(s2)((a, b) => a + b).take(4).toList === List(5, 7, 9))
+  }
+
+  test("startsWith: should return true if s2 is prefix of s1") {
+    val s1: Stream[Int] = cons(1, cons(2, cons(3, empty)))
+    val s2: Stream[Int] = cons(1, cons(2, empty))
+    assert(s1.startsWith(s2) === true)
+  }
+
+  test("startsWith: should return false if s2 is not prefix of s1") {
+    val s1: Stream[Int] = cons(1, cons(2, cons(3, empty)))
+    val s2: Stream[Int] = cons(1, cons(2, cons(3, cons(4, empty))))
+    assert(s1.startsWith(s2) === false)
+  }
+
+  test("tails: should return stream of suffics") {
+    val s1: Stream[Int] = cons(1, cons(2, cons(3, empty)))
+    assert(s1.tails.toList.map(_.toList) === List(List(1, 2, 3), List(2, 3), List(3)))
+  }
+
+  test("scanRight: should return stream of intermediate results") {
+    val s1: Stream[Int] = cons(1, cons(2, cons(3, empty)))
+    assert(s1.scanRight(0)(_ + _).toList === List(6, 5, 3, 0))
+  }
+
 
 }
